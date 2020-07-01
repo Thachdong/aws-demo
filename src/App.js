1830fffff1941bb78d199ca556780d3d68ad8978
Dongthach <thachdong270293@gmail.com>
2020-07-01 16:06:02 +0700

    added auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
       <h3>Hello from app</h3>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
